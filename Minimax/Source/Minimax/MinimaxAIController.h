691b7455e1aa67c4199b8e22071faa1c2797b31b
Andrea Agostini <andreaagostini1998@gmail.com>
2024-03-15 23:25:00 +0100

    Created MinimaxAIController

---
+++ Minimax/Source/Minimax/MinimaxAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MinimaxAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINIMAX_API AMinimaxAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
